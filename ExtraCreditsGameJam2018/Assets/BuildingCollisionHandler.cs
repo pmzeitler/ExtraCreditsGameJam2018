b60dae51f651f5ed4016ae0dff823da346646b69
Phoebe Zeitler (Morgana) <phoebe.zeitler@gmail.com>
2018-08-17 20:39:57 -0400

    Destroying ze missiles

---
+++ ExtraCreditsGameJam2018/Assets/BuildingCollisionHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingCollisionHandler : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("Colliding with object " + collision.collider.gameObject.name + "...");
+
+        if (collision.collider.gameObject.tag == "Missile")
+        {
+            Debug.Log("Destroying ze missile");
+            Destroy(collision.collider.gameObject);
+        }
+    }
+
+
+
+
+}
