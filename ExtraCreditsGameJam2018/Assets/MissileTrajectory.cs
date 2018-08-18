3c6656e9b76c06ab04c4e6ad6bed93e45109a506
Phoebe Zeitler (Morgana) <phoebe.zeitler@gmail.com>
2018-08-17 20:23:26 -0400

    Missile Guidance :)

---
+++ ExtraCreditsGameJam2018/Assets/MissileTrajectory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileTrajectory : MonoBehaviour {
+
+    public Vector2 InitialPosition
+    {
+        get; set;
+    }
+
+    public float SpeedPerFrame = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x + SpeedPerFrame,
+            gameObject.transform.position.y, gameObject.transform.position.z);
+		
+	}
+}
